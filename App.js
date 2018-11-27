ecc18111dea8b03a3873af9bd9bb258cb598f303
Admin <smorykon@s-pro.io>
2018-11-27 23:40:28 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import { createStackNavigator, createNavigationContainer, createBottomTabNavigator } from 'react-navigation';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+
+class Home extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title="Go" onPress={() => this.props.navigation.navigate('Card')}/>
+      </View>
+    );
+  }
+}
+
+const HomeStack = createStackNavigator({
+  Home,
+  Card: {
+    screen: Home
+  }
+});
+
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeStack,
+  Settings: Home,
+});
+
+const Navigator = createNavigationContainer(TabNavigator);
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Navigator />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
